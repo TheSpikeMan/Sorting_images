763e9608afc2ce8e2b2fb0035d2ea1fa25254392
TheSpikeMan <grzegorz.noga.94@gmail.com>
2024-11-11 23:08:18 +0100

    Modyfied 'find_image_video_files'. Added counter. Changed logic.

--- Sorting_images_PyQt6_v.2.0.py
+++ Sorting_images_PyQt6_v.2.0.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import re
 def find_image_video_files(path):
     """
-    Function searches for .jpg and .mp4 files in specific path folder.
-    For now it considers only files, not subdirectories.
+    Function searches for .jpg/.jpeg and .mp4 files in specific path folder.
+    It searches for files in path folder and all subdirectories.
 
     Parameters
     ----------
@@ -12,31 +12,43 @@ def find_image_video_files(path):
 
     Returns
     -------
-    files_list: type LIST, list of .jpg/.jpeg and .mp4 files in 'path' folder
+    matches_list: type LIST, list of .jpg/.jpeg and .mp4 files in 'path' folder
+    non_matches_list: type LIST, list of files not matching
     date_part: type LIST, list of all year-months combinations
     """
 
+    # Define all variables and objects
     files_list = []
     non_matches_list = []
     date_part = []
     path = Path(path)
-    for index, file in enumerate(path.rglob('*')):
-        if file.is_file():
-            print(index, ", ", file)
-            pattern = r'.*(?P<year>20[12]\d)(?P<month>[01]\d)(?P<day>[0123]\d).*'
-            if file.suffix in {'.jpg', '.mp4', '.jpeg'}:
-                # Check if match exists
-                matchObj = re.match(pattern=pattern, string=file.stem)
-                if matchObj:
-                    # If match exists add what's matched to date_part and the whole file to files_list
-                    date_part.append(matchObj.group('year') + '-' + \
-                                     matchObj.group('month'))
-                    files_list.append(file.name)
-                else:
-                    # If no match add file name to non_matches_lsit
-                    non_matches_list.append(file.name)
-                    continue
+
+    # Define counters to count found images and movies
+    counter = 0
+    counter_match = 0
+
+    # Define pattern and extensions to search
+    pattern = r'.*(?P<year>20[12]\d)(?P<month>[01]\d)(?P<day>[0123]\d).*'
+    extensions_to_search = ['*.jpg', '*.jpeg', '*.mp4']
+
+    for extension in extensions_to_search:
+        for file in path.rglob(extension):
+            counter = counter + 1
+            # Check if match exists
+            matchObj = re.match(pattern=pattern, string=file.stem)
+            if matchObj:
+                # If match exists add what's matched to date_part and the whole file to files_list
+                date_part.append(matchObj.group('year') + '-' + \
+                                 matchObj.group('month'))
+                files_list.append(file.name)
+                counter_match = counter_match + 1
+            else:
+                # If no match add file name to non_matches_list
+                non_matches_list.append(file.name)
+                continue
     # Find unique year and month
     date_part = list(set(date_part))
+    print("Found",counter,"files from which", round(100 * counter_match/counter, 2), "% belong to pictures or movies.")
     # Return all filenames with match and unique year and month
-    return files_list, date_part
+    return matches_list, non_matches_list, date_part
+
