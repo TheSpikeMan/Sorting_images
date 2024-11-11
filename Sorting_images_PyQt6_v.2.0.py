2e4a5b976ecbed1b04c3b7b4397786ba4018991b
TheSpikeMan <grzegorz.noga.94@gmail.com>
2024-11-11 17:02:43 +0100

    Rozszerzenie zakresu działania funkcji o podfoldery wchodzące w skład danego folderu głównego. Funkcja przeszukuje teraz wszystkie pliki.

--- Sorting_images_PyQt6_v.2.0.py
+++ Sorting_images_PyQt6_v.2.0.py
@@ -20,24 +20,23 @@ def find_image_video_files(path):
     non_matches_list = []
     date_part = []
     path = Path(path)
-    for index, file in enumerate(path.iterdir()):
-        print(index, ", ", file)
-        pattern = r'.*(?P<year>20[12]\d)(?P<month>[01]\d)(?P<day>[0123]\d).*'
-        if file.suffix in {'.jpg', '.mp4', '.jpeg'}:
-            # Check if match exists
-            matchObj = re.match(pattern=pattern, string=file.stem)
-            if matchObj:
-                # If match exists add what's matched to date_part and the whole file to files_list
-                date_part.append(matchObj.group('year') + '-' + \
-                                 matchObj.group('month'))
-                files_list.append(file.name)
-            else:
-                # If no match add file name to non_matches_lsit
-                non_matches_list.append(file.name)
-                continue
+    for index, file in enumerate(path.rglob('*')):
+        if file.is_file():
+            print(index, ", ", file)
+            pattern = r'.*(?P<year>20[12]\d)(?P<month>[01]\d)(?P<day>[0123]\d).*'
+            if file.suffix in {'.jpg', '.mp4', '.jpeg'}:
+                # Check if match exists
+                matchObj = re.match(pattern=pattern, string=file.stem)
+                if matchObj:
+                    # If match exists add what's matched to date_part and the whole file to files_list
+                    date_part.append(matchObj.group('year') + '-' + \
+                                     matchObj.group('month'))
+                    files_list.append(file.name)
+                else:
+                    # If no match add file name to non_matches_lsit
+                    non_matches_list.append(file.name)
+                    continue
     # Find unique year and month
     date_part = list(set(date_part))
     # Return all filenames with match and unique year and month
     return files_list, date_part
-
-result = find_image_video_files("G:\\NTFS-Zdjęcia\\2024\\slub")
