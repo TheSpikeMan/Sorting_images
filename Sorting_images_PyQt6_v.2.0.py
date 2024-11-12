d51918cc0927bf24988472d24f19679614919f5b
TheSpikeMan <grzegorz.noga.94@gmail.com>
2024-11-12 23:04:32 +0100

    Dalsza budowa aplikacji/

--- Sorting_images_PyQt6_v.2.0.py
+++ Sorting_images_PyQt6_v.2.0.py
@@ -1,92 +1,101 @@
 import re
+import os
 import datetime
 from pathlib import Path
 
 class Folder:
-    def __init__(self, folder_path):
-        self.folder_path = Path(folder_path.replace("\\", "\\\\"))
+    def __init__(self, source_folder_path, destination_folder_path):
+        self.source_folder_path = Path(source_folder_path.replace("\\", "\\\\"))
+        self.destination_folder_path = Path(destination_folder_path.replace("\\", "\\\\"))
     def path_validation(self):
-
-        if self.folder_path.exists():
-            if self.folder_path.is_dir():
-                print("Walidacja ścieżki zakończona pomyślnie.")
-                return True
+        self.folder_paths = [self.source_folder_path, self.destination_folder_path]
+        for index, self.path in enumerate(self.folder_paths):
+            if self.path.exists():
+                if self.path.is_dir():
+                    print(f"Nr {index + 1} path validation successful.")
+                else:
+                    print(f"Nr {index + 1} path validation unsuccessful - path indicates file.")
+                    return False
             else:
-                print("Walidacja ścieżki zakończona niepomyślnie - ścieżka wskazuje na plik.")
+                print(f"Nr {index + 1} path validation unsuccessful - path does not exist.")
                 return False
-        else:
-            print("Walidacja ścieżki zakończona niepomyślnie - ścieżka nie istnieje.")
-            return False
-
+        return True
     def find_image_video_files(self):
-        """
-        Function searches for .jpg/.jpeg and .mp4 files in specific path folder.
-        It searches for files in path folder and all subdirectories.
 
-        Parameters
-        Returns
-        -------
-        matches_list: type LIST, list of .jpg/.jpeg and .mp4 files in 'path' folder
-        non_matches_list: type LIST, list of files not matching
-        date_part: type LIST, list of all year-months combinations
-
-        """
         # Define all variables and objects
-        matches_list = []
-        non_matches_list = []
-        date_part = []
+        self.matches_list = []
+        self.non_matches_list = []
+        self.date_part = []
 
         # Define counters to count found images and movies
-        counter = 0
-        counter_match = 0
+        self.counter = 0
+        self.counter_match = 0
 
         # Define pattern and extensions to search
-        pattern = r'.*(?P<year>20[123]\d)(?P<month>[01]\d)(?P<day>[0123]\d).*'
-        extensions_to_search = ['*.jpg', '*.jpeg', '*.mp4']
+        self.pattern = r'.*(?P<year>20[123]\d)(?P<month>[01]\d)(?P<day>[0123]\d).*'
+        self.extensions_to_search = ['*.jpg', '*.jpeg', '*.mp4']
 
-        for extension in extensions_to_search:
+        for extension in self.extensions_to_search:
             try:
-                for file in self.folder_path.rglob(extension):
-                    counter += 1
+                for file in self.source_folder_path.rglob(extension):
+                    self.counter += 1
                     # Check if match exists
-                    match_obj = re.match(pattern=pattern, string=file.stem)
-                    if match_obj:
-                        year = int(match_obj.group('year'))
-                        month = int(match_obj.group('month'))
-                        day = int(match_obj.group('day'))
+                    self.match_obj = re.match(pattern=self.pattern, string=file.stem)
+                    if self.match_obj:
+                        self.year = int(self.match_obj.group('year'))
+                        self.month = int(self.match_obj.group('month'))
+                        self.day = int(self.match_obj.group('day'))
                         try:
                             # Try to create date object (additional date validation)
-                            datetime.date(year, month, day)
+                            datetime.date(self.year, self.month, self.day)
 
                             # Add what's matched to date_part and the whole file to matches_list
-                            date_part.append(f"{year}-{month:02}")
-                            matches_list.append(file.name)
-                            counter_match += 1
+                            self.date_part.append(f"{self.year}-{self.month:02}")
+                            self.matches_list.append(file.name)
+                            self.counter_match += 1
                         except ValueError:
                             continue
                     else:
                         # If no match add file name to non_matches_list
-                        non_matches_list.append(file.name)
+                        self.non_matches_list.append(file.name)
             except Exception as e:
                 print(f"Error processing files: {e}")
         # Find unique year and month
-        date_part = list(set(date_part))
+        self.date_part = list(set(self.date_part))
 
         # Write information about files matches and found
-        if counter > 0:
-            print("Found", counter, "files from which", round(100 * counter_match / counter, 2),
+        if self.counter > 0:
+            print("Found", self.counter, "files from which", round(100 * self.counter_match / self.counter, 2),
                   "% belong to pictures or movies.")
         else:
             print("No files found.")
         # Return all filenames with match and unique year and month
-        return matches_list, non_matches_list, date_part
+        return self.matches_list, self.non_matches_list, self.date_part
+    def search_for_copies(self, matches_list):
+        self.list_of_copies = []
+        for picture in matches_list:
+            for file in self.destination_folder_path.rglob(picture):
+                # If file has been found add it to the list
+                self.list_of_copies.append(file.name)
+        return self.list_of_copies
+
+    def find_files_to_copy(self, list_of_copies, matches_list ):
+        self.files_to_copy = list(set(matches_list) - set(list_of_copies))
+        print(f"Among {self.counter_match} files {len(self.files_to_copy)} of them are to be copied.")
 
     def run(self):
         validation_flag = self.path_validation()
         # if path is correct
         if validation_flag:
-            self.find_image_video_files()
+            matches_list, non_matches_list, date_part = self.find_image_video_files()
+            list_of_copies = self.search_for_copies(matches_list)
+            self.find_files_to_copy(list_of_copies, matches_list)
 
-# Definiowanie ścieżki
-folder_object = Folder(r'G:\NTFS-Zdjęcia\Test_folder')
-folder_object.run()
+# Main part
+if True:
+    # Paths definitions
+    source_folder_path = r''
+    destination_folder_path = r''
+
+    object = Folder(source_folder_path, destination_folder_path)
+    object.run()
