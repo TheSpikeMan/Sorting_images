a41356081083174cb4991cfd69b4a2e1fb001206
TheSpikeMan <grzegorz.noga.94@gmail.com>
2024-11-12 21:36:40 +0100

    Dalsze poprawki, walidacje.

--- Sorting_images_PyQt6_v.2.0.py
+++ Sorting_images_PyQt6_v.2.0.py
@@ -1,9 +1,10 @@
 import re
 import datetime
 from pathlib import Path
+
 class Folder:
     def __init__(self, folder_path):
-        self.folder_path = Path(folder_path)
+        self.folder_path = Path(folder_path.replace("\\", "\\\\"))
     def path_validation(self):
 
         if self.folder_path.exists():
@@ -44,30 +45,39 @@ class Folder:
         extensions_to_search = ['*.jpg', '*.jpeg', '*.mp4']
 
         for extension in extensions_to_search:
-            for file in self.folder_path.rglob(extension):
-                counter = counter + 1
-                # Check if match exists
-                matchObj = re.match(pattern=pattern, string=file.stem)
-                if matchObj:
-                    year = int(matchObj.group('year'))
-                    month = int(matchObj.group('month'))
-                    day = int(matchObj.group('day'))
-                    try:
-                        # Try to create date object (additional date validation)
-                        datetime.date(year, month, day)
-                        # Add what's matched to date_part and the whole file to matches_list
-                        date_part.append(str(year) + '-' + str(month))
-                        matches_list.append(file.name)
-                        counter_match = counter_match + 1
-                    except ValueError:
-                        continue
-                else:
-                    # If no match add file name to non_matches_list
-                    non_matches_list.append(file.name)
+            try:
+                for file in self.folder_path.rglob(extension):
+                    counter += 1
+                    # Check if match exists
+                    match_obj = re.match(pattern=pattern, string=file.stem)
+                    if match_obj:
+                        year = int(match_obj.group('year'))
+                        month = int(match_obj.group('month'))
+                        day = int(match_obj.group('day'))
+                        try:
+                            # Try to create date object (additional date validation)
+                            datetime.date(year, month, day)
+
+                            # Add what's matched to date_part and the whole file to matches_list
+                            date_part.append(f"{year}-{month:02}")
+                            matches_list.append(file.name)
+                            counter_match += 1
+                        except ValueError:
+                            continue
+                    else:
+                        # If no match add file name to non_matches_list
+                        non_matches_list.append(file.name)
+            except Exception as e:
+                print(f"Error processing files: {e}")
         # Find unique year and month
         date_part = list(set(date_part))
-        print("Found", counter, "files from which", round(100 * counter_match / counter, 2),
-              "% belong to pictures or movies.")
+
+        # Write information about files matches and found
+        if counter > 0:
+            print("Found", counter, "files from which", round(100 * counter_match / counter, 2),
+                  "% belong to pictures or movies.")
+        else:
+            print("No files found.")
         # Return all filenames with match and unique year and month
         return matches_list, non_matches_list, date_part
 
@@ -77,6 +87,6 @@ class Folder:
         if validation_flag:
             self.find_image_video_files()
 
-
-folder_object = Folder("G:\\NTFS-Zdjęcia")
+# Definiowanie ścieżki
+folder_object = Folder(r'G:\NTFS-Zdjęcia\Test_folder')
 folder_object.run()
